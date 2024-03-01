bbeb37dbb473a875ee8fe46592467db0c2262d0d
Saifsiddiqui1630 <161847569+Saifsiddiqui1630@users.noreply.github.com>
2024-03-01 19:20:49 +0530

    Add files via upload

---
+++ PORTFOLIO/main.js
@@ -0,0 +1,26 @@
+
+// // When the user scrolls the page, execute myFunction
+// window.onscroll = function() {myFunction()};
+
+// function myFunction() {
+//   var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+//   var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+//   var scrolled = (winScroll / height) * 100;
+//   document.getElementById("myBar").style.height = scrolled + "%";
+// }
+
+// //navbar on scroll
+// const navEl= document.querySelector('.navbar');
+// function fun(){
+//   navEl.className.remove('navbar-scrolled');
+// }
+// window.addEventListener('scroll',(e) =>{
+//   console.log("hdsfjk"+navEl.className)
+//   if (window.scrollY >= 56) {
+//     navEl.className.add('navbar-scrolled');
+//   }
+//   else if (window.scrollY < 56){
+//     navEl.className.remove('navbar-scrolled');
+//   }
+// });
+
